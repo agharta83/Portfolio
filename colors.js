1cd161c3d7fd486ef02d3d0a63663e8318ea2b90
agharta83 <35310694+agharta83@users.noreply.github.com>
2019-10-23 22:41:58 +0200

    custom color palette

--- colors.js
+++ colors.js
@@ -1,12 +1,12 @@
 module.exports = {
-  background: '#FFFFFF',
-  backgroundDark: '#f0e6f6',
+  background: '#FFFFFF', // white
+  backgroundDark: '#dcdcdc', // lightgrey
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#6B6CCF', // Title font Landing Section
+  primaryLight: '#6C6DD0', // Light Purple/Blue on highlight About me section
+  primaryDark: '#474eac', // Dark blue
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#ff8490', // Orange
+  secondaryLight: '#FF9389', // Background Orange Projects section
+  secondaryDark: '#FF8363', // Title section
 };
